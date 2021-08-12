91d77aa047c6a8ab66383110c444037b3cc7be3f
ezequielW <ezequielwokraczka@gmail.com>
2021-08-11 23:01:24 -0300

    Improved performance by resizing images and replacing cdns

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
-$(function () {
-  	$(document).scroll(function () {
-	  	const nav = document.getElementById("mainNavbar");
-		$(nav).toggleClass('scrolled', $(this).scrollTop() > $(nav).height());
-		if($(this).scrollTop() > $(nav).height()){
-			nav.classList.remove('navbar-dark');
-			nav.classList.add('navbar-light');
-		}
-		else{
-			nav.classList.remove('navbar-light');
-			nav.classList.add('navbar-dark');
-		}
-	});
-});
+const toggleAppbar = () => {
+	const nav = document.getElementById("mainNavbar");
+	if(window.pageYOffset < nav.offsetHeight){
+		nav.classList.remove('scrolled');
+		nav.classList.remove('navbar-light');
+		nav.classList.add('navbar-dark');
+	}
+	else{
+		nav.classList.add('scrolled');
+		nav.classList.remove('navbar-dark');
+		nav.classList.add('navbar-light');
+	}
+}
+
+window.addEventListener('scroll', toggleAppbar);
